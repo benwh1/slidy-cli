1b2da4a79354fe2d672ea4fa830fc02dc5b6cbcc
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2022-12-14 00:15:35 +0000

    Add basic arg parser with `clap`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(
+    author, version, about, long_about = None,
+    propagate_version = true,
+    subcommand_required = true,
+    arg_required_else_help = true,
+)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
